0ae48eda76080e79209830ac710c965d51bc43f7
Mainulhasan05 <mdrifatbd5@gmail.com>
2022-11-19 20:42:12 +0600

    msg

--- index.js
+++ index.js
@@ -14,33 +14,70 @@ function myFun(){
             banglaText.innerText=data[rand].text
             author[0].innerText="-"+data[rand].author
             author[1].innerText="-"+data[rand].author
+            myFun2(data[rand].text)
+
             
         })
     })
 }
 myFun()
 
-function myFun2(){
-const encodedParams = new URLSearchParams();
-encodedParams.append("q", "Hello, world!");
-encodedParams.append("target", "bn");
-encodedParams.append("source", "en");
-
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'Accept-Encoding': 'application/gzip',
-		'X-RapidAPI-Key': 'f4ed7ea626msh87898269fa74339p1cae5bjsn4b0488f8a588',
-		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-	},
-	body: encodedParams
-};
-
-fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+const myFun2=async(msg)=>{
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("q", msg);
+    encodedParams.append("target", "bn");
+    encodedParams.append("source", "en");
+    
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'application/gzip',
+            'X-RapidAPI-Key': 'b555427a5fmsh420b94068cfe858p1ac427jsn68ecd23603c2',
+            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        },
+        body: encodedParams
+    };
+    
+    fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(response => {
+        banglaText.innerText=response.data.translations[0].translatedText
+        
+    })
+	.catch(err =>{
+        if(err){
+            
+        }
+    } );
+
 }
 
 
+
+
+
+
+// const encodedParams = new URLSearchParams();
+// encodedParams.append("q", msg);
+// encodedParams.append("target", "bn");
+// encodedParams.append("source", "en");
+
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'Accept-Encoding': 'application/gzip',
+// 		'X-RapidAPI-Key': 'f4ed7ea626msh87898269fa74339p1cae5bjsn4b0488f8a588',
+// 		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+// 	},
+// 	body: encodedParams
+// };
+
+// fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+// 	.then(response => response.json())
+// 	.then(response => {
+//         banglaText.innerText=response.data.translations[0].translatedText
+        
+//     })
+// 	.catch(err => console.error(err));
